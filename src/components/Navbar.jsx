866f6047bf35499a128297ac5406eaf074a8160e
JesseeGatimu <gakuojessee@gmail.com>
2026-07-20 09:36:27 +0300

    Created the Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
+
+
+const Navbar =() =>{
+    const [nav, setNav]=useState(false);
+    return(
+        <div className=" flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+            <h1 className="w-full text-3xl font-bold text-[#95edec]">GATIMU.dev</h1>
+            <ul className="hiden md:flex text-[#95edec]">
+                <li className="p-4">Home</li>
+                <li className="p-4">Skills</li>
+                <li className="p-4">About</li>
+                <li className="p-4">Contact</li>
+            </ul>
+            <div className="md:hidden" onClick={()=>setNav(!nav)}>
+                {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+            </div>
+            <div className={nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"}>
+              <h1 className="w-full text-3xl font-bold text-[#95edec] p-4">GATIMU.dev</h1>
+              <ul className="uppercase p-4 text-[#95edec]">
+                <li className="p-4 border-b border-gray-600">Home</li>
+                <li className="p-4 border-b border-gray-600">Skills</li>
+                <li className="p-4 border-b border-gray-600">About</li>
+                <li className="p-4 border-b border-gray-600">Contact</li>
+              </ul>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
